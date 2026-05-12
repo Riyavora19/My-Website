b0a4f3919aa08daf64e13229c105b0757a2ae1f4
GK1503 <gopalkrishnasub1503@gmail.com>
2026-05-12 22:55:41 +0530

    fix

--- backend/server.js
+++ backend/server.js
@@ -14,6 +14,7 @@ const allowedOrigins = [
   "http://localhost:5173",
   "http://localhost:5174",
   process.env.CLIENT_URL,
+  "https://universityfeedbacktrackingsystem.vercel.app",
 ]
   .filter(Boolean)
   .map((origin) => origin.replace(/\/$/, ""));
